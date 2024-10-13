cce91ac37bebef7d65334f6725758adc96012d08
ashok205050 <ashokmaurya2050@gmail.com>
2024-10-13 14:19:14 +0530

    s

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,7 +9,7 @@ const ContactForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://127.0.0.1:8000/api/contact/', {
+    const response = await fetch('https://walli-django-production.up.railway.app/api/contact/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
