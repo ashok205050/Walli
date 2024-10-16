062b3b9e009b4135c2d6a5e0dbe0af8c3610b62f
ashok205050 <ashokmaurya2050@gmail.com>
2024-10-16 20:27:25 +0530

    s

--- src/components/ResetPasswordConfirm.js
+++ src/components/ResetPasswordConfirm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-// import './ResetPasswordConfirm.css';
+import './ResetPasswordConfirm.css';
 
 const ResetPasswordConfirm = () => {
   const { uid, token } = useParams();
