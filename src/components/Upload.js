817010e2f4b17802d7fe1fd507221a2c881c765f
ashok205050 <ashokmaurya2050@gmail.com>
2024-10-16 19:59:25 +0530

    s

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { storage } from './firebaseConfig'; 
+import './Upload.css';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 const Upload = () => {
